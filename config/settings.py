d44f3bda245a33bd66b91433b9974d7723464205
viktor <ivakhnenkovd@yandex.ru>
2021-09-22 14:56:49 +0300

    v 1.1.2

--- config/settings.py
+++ config/settings.py
@@ -121,4 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/',
+]
+MEDIA_URL = 'http://127.0.0.1:8000/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
